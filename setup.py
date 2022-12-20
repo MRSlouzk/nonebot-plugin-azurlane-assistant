34c4532aa2b83ae84a433903fa17c03f151ee4cc
MRSlou <79364553+MRSlouzk@users.noreply.github.com>
2022-12-20 17:19:12 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='nonebot-plugin-azurlane-assistant',
-    version='0.0.1',
+    version='0.0.1-fix',
     packages=['nonebot-plugin-azurlane-assistant', 'nonebot-plugin-azurlane-assistant.modules'],
     long_description=long_description,
     long_description_content_type="text/markdown",
